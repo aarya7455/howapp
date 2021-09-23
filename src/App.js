4f629725ba144addb74039223d49e4f935147927
aarya <dhimanaaarya22@gmail.com>
2021-09-23 07:14:33 -0700

    hello

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Test from './test'
+
+export default class App extends React.Component {
+    constructor()
+    {
+        super();
+        this.state={name:"aarya"}
+    }
+    upset(){
+        this.setState({name:"aditi"})
+    }
+    render(){
+        return(
+            <>
+            <h1>
+                HELLO {this.state.name}
+            </h1>
+            <button onClick={()=>{this.upset()}}>update</button>
+            </>
+        )
+    }
+
+}
